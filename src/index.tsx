4485ebf69f2c92c37209f4ca4d6f8c1b43659fc0
Moussa Iskounene <moussa.iskounene@gmail.com>
2021-12-05 22:07:38 +0100

    :tada: Start the project
    * :tada: Begin the project
    
    * :wrench: Add tsconfig.json file

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react';
+import { render } from 'react-dom';
+import Application from './app';
+
+const NODE_APP = document.getElementById('killerparty');
+
+render(
+  <StrictMode>
+    <Application />
+  </StrictMode>,
+  NODE_APP,
+);
---
+++ utils/react-shim.ts
@@ -0,0 +1,10 @@
+import React from 'react';
+
+/**
+ * The purpose of this file is to use JSX syntax without importing `React`
+ * with the built-in `inject` feature in esbuild.
+ *
+ * @link https://esbuild.github.io/content-types/#auto-import-for-jsx
+ */
+
+export { React };
