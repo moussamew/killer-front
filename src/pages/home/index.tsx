a06a596964c2477a7fad61936bd21940b1893235
Moussa Iskounene <moussa.iskounene@gmail.com>
2022-02-08 01:35:34 +0100

    :lipstick: Use style inside home page component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,6 @@
 import { Fragment, useContext, useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import tw from 'tailwind-styled-components';
 
 import Killerparty from '../../assets/images/killerparty.png';
 import { Button, Header, Input } from '../../components';
@@ -7,15 +8,37 @@ import { PlayerContext } from '../../hooks/context';
 import { t } from '../../translate/helpers';
 
 import { createPlayer, createRoom } from './requests';
-import {
-  Actions,
-  Content,
-  ErrorMessage,
-  PseudoRow,
-  PseudoSection,
-  Text,
-  WelcomeImage,
-} from './styles';
+
+const Content = tw.div`
+  max-w-screen-lg m-auto
+  inset-0 px-2
+`;
+
+const WelcomeImage = tw.img`
+  m-auto
+`;
+
+const Text = tw.p`
+  my-2
+`;
+
+const PseudoSection = tw.section`
+  mt-2 mb-1
+`;
+
+const PseudoRow = tw.div`
+  mt-2 flex flex-row 
+  justify-center items-center
+`;
+
+const ErrorMessage = tw.p`
+  normal-case bg-red-200 text-red-500 
+  p-1 mt-1 rounded-md text-2xl font-bold
+`;
+
+const Actions = tw.div`
+  mt-1
+`;
 
 const Home = (): JSX.Element => {
   const [errorMessage, setErrorMessage] = useState('');
