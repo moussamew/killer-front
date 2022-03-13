5901f649bfc76bc8ff8c906acb3ceb620848194f
Moussa Iskounene <moussa.iskounene@gmail.com>
2022-03-13 15:44:37 +0100

    :art: Create Modal component

---
+++ src/components/Modal.tsx
@@ -0,0 +1,38 @@
+import { ReactNode } from 'react';
+import tw from 'tailwind-styled-components';
+
+import Close from 'assets/icons/close.svg';
+
+const Background = tw.div`
+  bg-disabled absolute top-0
+  w-full h-full
+`;
+
+const Content = tw.div`
+  relative bg-brand m-auto
+  p-2 mx-2 top-1/3 
+  md:mx-1/4 lg:mx-1/3 xl:mx-2/5
+`;
+
+const CloseIcon = tw.img`
+  absolute cursor-pointer h-3
+  -top-1 -right-1
+`;
+
+interface Props {
+  children: ReactNode;
+  closeModal: () => void;
+}
+
+const Modal = ({ children, closeModal }: Props): JSX.Element => {
+  return (
+    <Background>
+      <Content>
+        <CloseIcon alt="close" src={Close} onClick={closeModal} />
+        {children}
+      </Content>
+    </Background>
+  );
+};
+
+export default Modal;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
 import Button from './Button';
 import Input from './Input';
-import Layout from './Layout';
 import Loader from './Loader';
+import Modal from './Modal';
 
-export { Button, Layout, Input, Loader };
+export { Button, Input, Loader, Modal };
