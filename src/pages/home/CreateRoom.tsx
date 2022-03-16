a624fe75a991a021a81ba9c6e3604b2774889bc5
Moussa Iskounene <moussa.iskounene@gmail.com>
2022-03-16 16:56:42 +0100

    :recycle: Simplify room creation

--- src/pages/home/CreateRoom.tsx
+++ src/pages/home/CreateRoom.tsx
@@ -1,12 +1,10 @@
 import { Fragment, RefObject, useContext, useState } from 'react';
-import { useQuery } from 'react-query';
 import { useNavigate } from 'react-router-dom';
 import tw from 'tailwind-styled-components';
 
 import { Button } from 'components';
 import t from 'helpers/translate';
 import { PlayerContext } from 'hooks/context';
-import { Player, Room } from 'types';
 
 import { createPlayer, createRoom } from './services/requests';
 
@@ -27,50 +25,32 @@ const CreateRoom = ({ inputPseudo, inputPseudoRef }: Props): JSX.Element => {
 
   const navigate = useNavigate();
 
-  const { refetch: queryCreatePlayer } = useQuery<Player, Error>(
-    'createPlayer',
-    () => createPlayer(inputPseudo),
-    {
-      enabled: false,
-    },
-  );
-
-  const { refetch: queryCreateRoom } = useQuery<Room, Error>(
-    'createRoom',
-    createRoom,
-    {
-      enabled: false,
-    },
-  );
-
   const showErrorMessage = (message: string): void => {
     setErrorMessage(message);
     inputPseudoRef.current?.focus();
   };
 
-  const handleRoomCreation = async (): Promise<void> => {
+  const handleRoomCreation = (): Promise<void> =>
+    createRoom()
+      .then(async (newRoom) => {
+        await refreshPlayerSession();
+        navigate(`/room/${newRoom.code}`);
+      })
+      .catch(() => showErrorMessage(t('home.create_room_error')));
+
+  const createNewRoom = async (): Promise<void> => {
     if (!playerSession.name) {
-      const { error: errorPlayer } = await queryCreatePlayer();
-
-      if (errorPlayer) {
-        return showErrorMessage(errorPlayer.message);
-      }
+      return createPlayer(inputPseudo)
+        .then(handleRoomCreation)
+        .catch((error) => showErrorMessage(error.message));
     }
 
-    const { error: errorRoom, data: newRoom } = await queryCreateRoom();
-
-    if (errorRoom || !newRoom) {
-      return showErrorMessage(t('home.create_room_error'));
-    }
-
-    await refreshPlayerSession();
-
-    return navigate(`/room/${newRoom.code}`);
+    return handleRoomCreation();
   };
 
   return (
     <Fragment>
-      <Button buttonColor="bg-red-400" onClick={handleRoomCreation}>
+      <Button buttonColor="bg-red-400" onClick={createNewRoom}>
         {t('home.create_room')}
       </Button>
       {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
