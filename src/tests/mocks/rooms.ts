a9fe92baf484691ecc31f63a62fe659c5076ba71
Moussa Iskounene <moussa.iskounene@gmail.com>
2023-01-20 02:15:28 +0100

    :clown_face: (mocks) add new mocks for players and rooms

---
+++ src/tests/mocks/rooms.ts
@@ -0,0 +1,19 @@
+export const pendingRoom = {
+  id: 17,
+  code: 'SOSPC',
+  name: "TRINITY's room",
+  status: 'PENDING',
+  players: [
+    {
+      id: 28,
+      name: 'TRINITY',
+      status: 'ALIVE',
+    },
+  ],
+  missions: [],
+  admin: {
+    id: 28,
+    name: 'TRINITY',
+    status: 'ALIVE',
+  },
+};
