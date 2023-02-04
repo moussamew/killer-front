7643ddacacef1361bf332f66f10ced120452b499
Moussa Iskounene <moussa.iskounene@gmail.com>
2023-02-04 16:59:19 +0100

    :lipstick: (landing) start landing page user interface

--- source/pages/Landing/index.tsx
+++ source/pages/Landing/index.tsx
@@ -1,3 +1,34 @@
+import styles from './index.module.css';
+
 export function Landing(): JSX.Element {
-  return <h1>dd</h1>;
+  return (
+    <div className={styles.content}>
+      <h1>Un petit meurtre entre amis.. Ça vous tente ?</h1>
+      <div className={styles.textDescription}>
+        <h3>Tuer ou être tuer, faites votre choix.</h3>
+        <p>
+          Killerparty est un jeu en ligne où vous devez demasquez vos ennemis
+        </p>
+        <p>
+          avant de vous faire tuer. Vous pouvez être la proie comme le
+          prédateur.
+        </p>
+      </div>
+      <div className={styles.actions}>
+        <button className={styles.button} type="button" onClick={() => {}}>
+          Créer une nouvelle partie
+        </button>
+        <button className={styles.button} type="button" onClick={() => {}}>
+          Rejoindre une partie
+        </button>
+        {/*  <Button content="Rejoindre une partie" onClick={() => {}} />
+        <Button
+          content="Créer une nouvelle partie"
+          onClick={() => {}}
+          buttonColor="yellow"
+          textColor="lightDark"
+        /> */}
+      </div>
+    </div>
+  );
 }
