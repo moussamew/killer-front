da9a4350e65e627ca43efe3e29c1242e6655e7c7
Moussa Iskounene <moussa.iskounene@gmail.com>
2023-03-27 00:00:05 +0200

    Remove event source in playing room page

--- source/pages/Room/Playing/index.tsx
+++ source/pages/Room/Playing/index.tsx
@@ -1,9 +1,3 @@
-import { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-
-import { PROD_ENV } from '@/constants/app';
-import { ROOM_TOPIC } from '@/constants/endpoints';
-import { MercureEventType } from '@/constants/enums';
 import { PlayerList } from '@/pages/Room/Pending/PlayerList';
 import { PlayerStatus } from '@/services/player/constants';
 import { useSession } from '@/services/player/queries';
@@ -13,28 +7,7 @@ import { Status } from './Status';
 import styles from './styles/index.module.css';
 
 export function PlayingRoomPage(): JSX.Element {
-  const { roomCode } = useParams();
-  const { session, refetchSession } = useSession();
-
-  /**
-   * Listen to SSE events emits in the Room page.
-   * Apply corresponding side-effects.
-   */
-  useEffect(() => {
-    const roomEventSource = new EventSource(`${ROOM_TOPIC}/${roomCode}`, {
-      withCredentials: PROD_ENV,
-    });
-
-    roomEventSource.addEventListener('message', (event) => {
-      const { type } = JSON.parse(event.data);
-
-      if (type === MercureEventType.PLAYER_KILLED) {
-        refetchSession();
-      }
-    });
-
-    return () => roomEventSource.close();
-  }, [roomCode, refetchSession]);
+  const { session } = useSession();
 
   return (
     <>
