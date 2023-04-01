245ec2811aefa146234f6f429cb97d2d6421af86
Moussa Iskounene <moussa.iskounene@gmail.com>
2023-04-01 15:20:41 +0200

    Fix flacky test in JoinRoomModal component

--- source/pages/Home/__tests__/JoinRoomModal.spec.tsx
+++ source/pages/Home/__tests__/JoinRoomModal.spec.tsx
@@ -71,8 +71,6 @@ describe('<JoinRoomModal />', () => {
       roomCode,
     );
 
-    await userEvent.click(screen.getByText('Rejoindre cette partie'));
-
     server.use(
       rest.get(SESSION_ENDPOINT, (_, res, ctx) =>
         res(ctx.status(200), ctx.json(pendingRoomSession)),
@@ -81,6 +79,8 @@ describe('<JoinRoomModal />', () => {
         res(ctx.status(200), ctx.json(pendingRoom)),
       ),
     );
+
+    await userEvent.click(screen.getByText('Rejoindre cette partie'));
 
     await screen.findByText('Bienvenue à la fête !');
 
