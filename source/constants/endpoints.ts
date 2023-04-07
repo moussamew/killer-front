5844adde9cf399f567ad23cb8819bcb072483320
Moussa Iskounene <moussa.iskounene@gmail.com>
2023-04-07 20:14:09 +0200

    Remove VITE_FRONT_URL env variable

--- source/constants/endpoints.ts
+++ source/constants/endpoints.ts
@@ -1,4 +1,4 @@
-import { API_URL, FRONT_URL, MERCURE_URL } from './app';
+import { API_URL, MERCURE_URL } from './app';
 
 /**
  * --- ENDPOINTS ---
@@ -12,8 +12,3 @@ export const MISSION_ENDPOINT = `${API_URL}/mission`;
  * --- TOPICS (SSE)  ---
  */
 export const ROOM_TOPIC = `${MERCURE_URL}?topic=room`;
-
-/**
- * --- ROUTES ---
- */
-export const JOIN_ROOM_ROUTE = `${FRONT_URL}/join`;
--- source/types/vite.d.ts
+++ source/types/vite.d.ts
@@ -4,7 +4,6 @@
 interface ImportMetaEnv {
   readonly VITE_API_URL: string;
   readonly VITE_MERCURE_URL: string;
-  readonly VITE_FRONT_URL: string;
 }
 
 interface ImportMeta {
