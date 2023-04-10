6133036a91e40db37ceadf771d521b0888a6a095
Moussa Iskounene <moussa.iskounene@gmail.com>
2023-04-10 13:26:23 +0200

    Fix play another party button

--- source/pages/Room/Ended/index.tsx
+++ source/pages/Room/Ended/index.tsx
@@ -1,9 +1,8 @@
 import { useTranslation } from 'react-i18next';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 import Winner from '@/assets/images/winner.png';
 import { Button } from '@/components/Button';
-import { PlayerStatus } from '@/services/player/constants';
 import { useUpdatePlayer } from '@/services/player/mutations';
 import { useSession } from '@/services/player/queries';
 import { useRoom } from '@/services/room/queries';
@@ -13,26 +12,18 @@ import styles from './styles/index.module.css';
 export function EndedRoomPage(): JSX.Element {
   const { roomCode } = useParams();
   const { t } = useTranslation();
-  const { room } = useRoom(roomCode!);
   const { updatePlayer } = useUpdatePlayer();
   const { session } = useSession();
-  const navigate = useNavigate();
+  const { room } = useRoom(roomCode!);
 
   const handleLeaveRoom = async (): Promise<void> => {
-    updatePlayer.mutate(
-      { id: session?.id, room: null },
-      { onSuccess: () => navigate('/') },
-    );
+    await updatePlayer.mutateAsync({ id: session?.id, room: null });
   };
-
-  const lastManStanding = room?.players?.find(
-    ({ status }) => status === PlayerStatus.ALIVE,
-  );
 
   return (
     <>
       <div className={styles.infos}>
-        <h1>{t('room.winner.name', { playerName: lastManStanding?.name })}</h1>
+        <h1>{t('room.winner.name', { playerName: room?.winner?.name })}</h1>
         <p>{t('room.winner.congrats')}</p>
       </div>
       <img alt="notFound" src={Winner} className={styles.image} />
