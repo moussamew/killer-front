b16b26ced2f268714e99692083e8b21f8f508660
Moussa Iskounene <moussa.iskounene@gmail.com>
2023-04-10 15:15:40 +0200

    Remove player kick modal and fix kick authorization

--- source/pages/Room/Pending/PlayerList.tsx
+++ source/pages/Room/Pending/PlayerList.tsx
@@ -8,10 +8,10 @@ import KickPlayer from '@/assets/icons/kickPlayer.svg';
 import LeaveRoom from '@/assets/icons/leaveRoom.svg';
 import Player from '@/assets/images/player.png';
 import { ModalContext } from '@/context/modal';
+import { useUpdatePlayer } from '@/services/player/mutations';
 import { useSession } from '@/services/player/queries';
 import { useRoom } from '@/services/room/queries';
 
-import { KickPlayerModal } from './KickPlayerModal';
 import { LeaveRoomModal } from './LeaveRoomModal';
 import { RoomSettings } from './RoomSettings';
 import styles from './styles/PlayerList.module.css';
@@ -20,6 +20,7 @@ export function PlayerList(): JSX.Element {
   const { roomCode } = useParams();
   const { room } = useRoom(roomCode!);
   const { session } = useSession();
+  const { updatePlayer } = useUpdatePlayer();
   const { openModal } = useContext(ModalContext);
   const { t } = useTranslation();
 
@@ -27,12 +28,9 @@ export function PlayerList(): JSX.Element {
     openModal(<LeaveRoomModal />);
   };
 
-  const handleKickPlayer =
-    (playerName: string, playerId: number) => (): void => {
-      openModal(
-        <KickPlayerModal playerName={playerName} playerId={playerId} />,
-      );
-    };
+  const handleKickPlayer = async (playerId: number): Promise<void> => {
+    await updatePlayer.mutateAsync({ id: playerId, room: null });
+  };
 
   return (
     <div className={styles.content}>
@@ -57,13 +55,15 @@ export function PlayerList(): JSX.Element {
               className={styles.icon}
             />
           )}
-          {room?.admin?.id !== id && session?.name !== name && (
-            <KickPlayer
-              title={t('tooltip.kick.player')}
-              onClick={handleKickPlayer(name, id)}
-              className={styles.icon}
-            />
-          )}
+          {room.admin.id === session?.id &&
+            room?.admin?.id !== id &&
+            session?.name !== name && (
+              <KickPlayer
+                title={t('tooltip.kick.player', { playerName: name })}
+                onClick={() => handleKickPlayer(id)}
+                className={styles.icon}
+              />
+            )}
         </div>
       ))}
     </div>
--- source/pages/Room/Pending/__tests__/PlayerList.test.tsx
+++ source/pages/Room/Pending/__tests__/PlayerList.test.tsx
@@ -1,10 +1,20 @@
-import { screen } from '@testing-library/react';
+import { screen, waitForElementToBeRemoved } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { sources } from 'eventsourcemock';
 import { t } from 'i18next';
 import { rest } from 'msw';
 
 import { SESSION_ENDPOINT, ROOM_ENDPOINT } from '@/constants/endpoints';
-import { fakePlayerThree, fakePlayerTwo } from '@/tests/mocks/players';
-import { pendingRoomWithMultiplePlayers, roomCode } from '@/tests/mocks/rooms';
+import {
+  fakePlayerOne,
+  fakePlayerThree,
+  fakePlayerTwo,
+} from '@/tests/mocks/players';
+import {
+  pendingRoomWithMultiplePlayers,
+  roomCode,
+  roomEventSource,
+} from '@/tests/mocks/rooms';
 import { pendingRoomSession } from '@/tests/mocks/sessions';
 import { renderWithProviders } from '@/tests/render';
 import { server } from '@/tests/server';
@@ -26,5 +36,48 @@ describe('<PlayerList />', () => {
 
     expect(screen.getByText(fakePlayerTwo.name)).toBeInTheDocument();
     expect(screen.getByText(fakePlayerThree.name)).toBeInTheDocument();
+  });
+
+  it('should kick player when clicking on kick player icon', async () => {
+    server.use(
+      rest.get(SESSION_ENDPOINT, (_, res, ctx) =>
+        res(ctx.status(200), ctx.json(pendingRoomSession)),
+      ),
+      rest.get(`${ROOM_ENDPOINT}/${roomCode}`, (_, res, ctx) =>
+        res(ctx.status(200), ctx.json(pendingRoomWithMultiplePlayers)),
+      ),
+    );
+
+    renderWithProviders({ route: `/room/${roomCode}` });
+
+    await screen.findByText(t('room.players.list'));
+
+    const newRoomInfos = {
+      ...pendingRoomWithMultiplePlayers,
+      players: [fakePlayerOne, fakePlayerThree],
+    };
+
+    server.use(
+      rest.get(`${ROOM_ENDPOINT}/${roomCode}`, (_, res, ctx) =>
+        res(ctx.status(200), ctx.json(newRoomInfos)),
+      ),
+    );
+
+    await userEvent.click(
+      screen.getByTitle(
+        t('tooltip.kick.player', { playerName: fakePlayerTwo.name }),
+      ),
+    );
+
+    sources[roomEventSource].emit(
+      'message',
+      new MessageEvent('message', {
+        data: JSON.stringify(newRoomInfos),
+      }),
+    );
+
+    await waitForElementToBeRemoved(() => screen.getByText(fakePlayerTwo.name));
+
+    expect(screen.queryByText(fakePlayerTwo.name)).not.toBeInTheDocument();
   });
 });
