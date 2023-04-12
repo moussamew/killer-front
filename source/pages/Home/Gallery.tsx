0e134e1b598dc768f98cba03aded6b31aeabb5a1
Moussa Iskounene <moussa.iskounene@gmail.com>
2023-04-12 20:51:20 +0200

    Use avatar for player list

--- source/pages/Home/Gallery.tsx
+++ source/pages/Home/Gallery.tsx
@@ -1,29 +1,11 @@
-import Avenger from '@/assets/images/avatars/avenger.svg';
-import Beach from '@/assets/images/avatars/beach.svg';
-import Captain from '@/assets/images/avatars/captain.svg';
-import Football from '@/assets/images/avatars/football.svg';
-import Gladiator from '@/assets/images/avatars/gladiator.svg';
-import Jedi from '@/assets/images/avatars/jedi.svg';
-import Milk from '@/assets/images/avatars/milk.svg';
-import Pirate from '@/assets/images/avatars/pirate.svg';
-import Samurai from '@/assets/images/avatars/samurai.svg';
-import Surf from '@/assets/images/avatars/surf.svg';
+import { AVATARS } from '@/constants/avatars';
 
 import styles from './styles/Gallery.module.css';
 
 export function Gallery(): JSX.Element {
   return (
     <div className={styles.gallery}>
-      <Samurai />
-      <Milk />
-      <Beach />
-      <Surf />
-      <Jedi />
-      <Captain />
-      <Gladiator />
-      <Football />
-      <Pirate />
-      <Avenger />
+      {Object.values(AVATARS).map((avatar) => avatar)}
     </div>
   );
 }
