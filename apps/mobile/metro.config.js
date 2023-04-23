afb48831d12e541d8eae2a8f849db4ae4f482f72
Moussa Iskounene <moussa.iskounene@gmail.com>
2023-04-23 20:13:08 +0200

    Add CSS modules support on mobile

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -10,12 +10,22 @@ const config = getDefaultConfig(projectRoot);
 
 // 1. Watch all files within the monorepo
 config.watchFolders = [workspaceRoot];
+
 // 2. Let Metro know where to resolve packages, and in what order
 config.resolver.nodeModulesPaths = [
   path.resolve(projectRoot, 'node_modules'),
   path.resolve(workspaceRoot, 'node_modules'),
 ];
+
 // 3. Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
 config.resolver.disableHierarchicalLookup = true;
 
-module.exports = config;
+module.exports = {
+  ...config,
+  transformer: {
+    babelTransformerPath: require.resolve('react-native-css-transformer'),
+  },
+  resolver: {
+    sourceExts: [...config.resolver.sourceExts, 'css'],
+  },
+};
