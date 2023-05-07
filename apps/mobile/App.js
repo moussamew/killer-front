b728fda2ce888ce7819f88a8d902d79a7364f34a
Moussa Iskounene <moussa.iskounene@gmail.com>
2023-05-07 15:52:31 +0200

    Create mobile landing page

--- apps/mobile/App.js
+++ apps/mobile/App.js
@@ -1,13 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Pressable, Text, ScrollView, View } from 'react-native';
+import {
+  SafeAreaProvider,
+  SafeAreaView,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
+import { setupIntl, useTranslation } from '@killerparty/intl';
 
+import { Gallery } from './components/Gallery';
 import styles from './index.module.css';
 
-export default function App() {
+import { Rules } from './Rules';
+
+import 'intl-pluralrules';
+
+setupIntl('fr-FR');
+
+const CustomStatusBar = () => {
+  const insets = useSafeAreaInsets();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your application!</Text>
-      <StatusBar style="auto" />
+    <View style={{ height: insets.top, backgroundColor: '#fdf7f2' }}>
+      <StatusBar animated={true} backgroundColor="#fdf7f2" />
     </View>
+  );
+};
+
+export default function App() {
+  const { t } = useTranslation();
+
+  return (
+    <SafeAreaProvider>
+      <CustomStatusBar />
+      <ScrollView style={styles.scroll}>
+        <View style={styles.view}>
+          <Text style={styles.title}>{t('home.title')}</Text>
+          <Text style={styles.subtitle}>{t('home.subtitle')}</Text>
+          <Text style={styles.description}>{t('home.description')}</Text>
+          <Pressable
+            style={({ pressed }) => [
+              styles.button,
+              styles.primary,
+              pressed && styles.primaryPressed,
+            ]}
+          >
+            <Text style={styles.primaryText}>
+              {t('home.create.room.button')}
+            </Text>
+          </Pressable>
+          <Pressable
+            style={({ pressed }) => [
+              styles.button,
+              styles.secondary,
+              pressed && styles.secondaryPressed,
+            ]}
+          >
+            <Text style={styles.secondaryText}>{t('home.join.room')}</Text>
+          </Pressable>
+          <Gallery />
+          <Rules />
+        </View>
+      </ScrollView>
+    </SafeAreaProvider>
   );
 }
