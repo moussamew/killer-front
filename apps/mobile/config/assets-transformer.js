0a21139e150ee63524ac3b1adf001fa7beb6fcf4
Moussa Iskounene <moussa.iskounene@gmail.com>
2023-05-07 15:51:37 +0200

    Update metro configuration

--- apps/mobile/config/assets-transformer.js
+++ apps/mobile/config/assets-transformer.js
@@ -1,10 +1,15 @@
 var upstreamTransformer = require('metro-react-native-babel-transformer');
 var postcssTransformer = require('react-native-postcss-transformer');
-var postCSSExtensions = ['css', 'pcss'];
+var svgTransformer = require('react-native-svg-transformer');
 
 module.exports.transform = function ({ src, filename, options }) {
-  if (postCSSExtensions.some((ext) => filename.endsWith('.' + ext))) {
+  if (['css', 'pcss'].some((ext) => filename.endsWith('.' + ext))) {
     return postcssTransformer.transform({ src, filename, options });
   }
+
+  if (filename.endsWith('.svg')) {
+    return svgTransformer.transform({ src, filename, options });
+  }
+
   return upstreamTransformer.transform({ src, filename, options });
 };
