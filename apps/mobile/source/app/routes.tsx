b6290c052656f3a7b6b4dc19d1ffc994b7a1c809
Moussa Iskounene <moussa.iskounene@gmail.com>
2023-07-04 23:11:53 +0200

    Create pending room page for mobile

--- apps/mobile/source/app/routes.tsx
+++ apps/mobile/source/app/routes.tsx
@@ -1,34 +1,57 @@
 import { t } from '@killerparty/intl';
+import { useSession } from '@killerparty/webservices';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Text, View } from 'react-native';
 
 import { CreateRoomPage } from '../pages/CreateRoom';
 import { HomePage } from '../pages/Home';
+import { PendingRoomPage } from '../pages/Room';
 
 export type RootStackParamList = {
   Home: undefined;
   CreateRoom: undefined;
+  Room: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export function Routes(): JSX.Element {
+  const { isLoading, session } = useSession();
+
+  if (isLoading) {
+    return (
+      <View>
+        <Text>Loading</Text>
+      </View>
+    );
+  }
+
   return (
     <Stack.Navigator
-      initialRouteName="Home"
       screenOptions={{
         headerStyle: { backgroundColor: '#fdf7f2' },
       }}
     >
-      <Stack.Screen
-        name="Home"
-        component={HomePage}
-        options={{ title: 'KILLER PARTY' }}
-      />
-      <Stack.Screen
-        name="CreateRoom"
-        component={CreateRoomPage}
-        options={{ title: t('create.room.page.title') }}
-      />
+      {session ? (
+        <Stack.Screen
+          name="Room"
+          component={PendingRoomPage}
+          options={{ title: t('room.welcome.title') }}
+        />
+      ) : (
+        <>
+          <Stack.Screen
+            name="Home"
+            component={HomePage}
+            options={{ title: 'KILLER PARTY' }}
+          />
+          <Stack.Screen
+            name="CreateRoom"
+            component={CreateRoomPage}
+            options={{ title: t('create.room.page.title') }}
+          />
+        </>
+      )}
     </Stack.Navigator>
   );
 }
