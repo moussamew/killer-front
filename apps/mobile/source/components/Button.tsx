256f92d7e8d2d8d36986bc7618c7274405279bed
Moussa Iskounene <moussa.iskounene@gmail.com>
2023-07-15 16:45:27 +0200

    Handle disabled button into <Button /> component

--- apps/mobile/source/components/Button.tsx
+++ apps/mobile/source/components/Button.tsx
@@ -6,16 +6,19 @@ interface Props {
   onPress: () => void;
   color: 'primary' | 'secondary';
   text: string;
+  disabled?: boolean;
 }
 
-export function Button({ onPress, color, text }: Props): JSX.Element {
+export function Button({ onPress, color, text, disabled }: Props): JSX.Element {
   return (
     <Pressable
+      disabled={disabled}
       style={({ pressed }) => [
         styles.button,
         styles[color],
         pressed && color === 'primary' && styles.primaryPressed,
         pressed && color === 'secondary' && styles.secondaryText,
+        disabled && styles.disabled,
       ]}
       onPress={onPress}
     >
