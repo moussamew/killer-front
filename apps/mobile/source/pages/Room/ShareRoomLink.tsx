d464c296148c6e642850d8dea312986f03ee735a
Moussa Iskounene <moussa.iskounene@gmail.com>
2023-07-15 16:39:47 +0200

    Create ShareRoomLink button

---
+++ apps/mobile/source/pages/Room/ShareRoomLink.tsx
@@ -0,0 +1,15 @@
+import { useTranslation } from '@killerparty/intl';
+
+import { Button } from '../../components/Button';
+
+export function ShareRoomLink(): JSX.Element {
+  const { t } = useTranslation();
+
+  return (
+    <Button
+      color="secondary"
+      onPress={() => {}}
+      text={t('room.share.link.button')}
+    />
+  );
+}
--- apps/mobile/source/pages/Room/index.tsx
+++ apps/mobile/source/pages/Room/index.tsx
@@ -2,6 +2,7 @@ import { useTranslation } from '@killerparty/intl';
 import { useSession } from '@killerparty/webservices';
 import { Text, View, Image } from 'react-native';
 
+import { ShareRoomLink } from './ShareRoomLink';
 import styles from './styles/index.module.css';
 
 export function PendingRoomPage(): JSX.Element | null {
@@ -15,10 +16,10 @@ export function PendingRoomPage(): JSX.Element | null {
         <Image source={require('../../assets/images/island.png')} />
         <View style={styles.description}>
           <Text style={styles.title}>{t('room.welcome.title')}</Text>
-          <Text>
+          <Text style={styles.joinRoomCode}>
             {t('room.join.room.code', { roomCode: session?.room?.id })}
           </Text>
-
+          <ShareRoomLink />
           {/* <ShareRoomLink />
           <StartPartyButton /> */}
         </View>
