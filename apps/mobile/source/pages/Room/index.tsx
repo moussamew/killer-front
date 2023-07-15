f0546f812052db9653fa38664b79a3f77d5fda94
Moussa Iskounene <moussa.iskounene@gmail.com>
2023-07-15 17:48:28 +0200

    Implement room missions component

--- apps/mobile/source/pages/Room/index.tsx
+++ apps/mobile/source/pages/Room/index.tsx
@@ -1,32 +1,37 @@
 import { useTranslation } from '@killerparty/intl';
-import { useSession } from '@killerparty/webservices';
+import { type NativeStackScreenProps } from '@react-navigation/native-stack';
 import { Text, View, Image } from 'react-native';
 
+import { type RootStackParamList } from '../../app/routes';
+
+import { RoomMissions } from './RoomMissions';
 import { ShareRoomLink } from './ShareRoomLink';
 import { StartPartyButton } from './StartPartyButton';
 import styles from './styles/index.module.css';
 
-export function PendingRoomPage(): JSX.Element | null {
-  const { session } = useSession();
+type Props = NativeStackScreenProps<RootStackParamList, 'Room'>;
+
+export function PendingRoomPage({ route }: Props): JSX.Element | null {
   const { t } = useTranslation();
+
+  const { roomCode } = route.params;
 
   return (
     <View style={styles.container}>
       <View style={styles.content}>
-        {/* eslint-disable-next-line global-require */}
-        <Image source={require('../../assets/images/island.png')} />
-        <View style={styles.description}>
-          <Text style={styles.title}>{t('room.welcome.title')}</Text>
-          <Text style={styles.joinRoomCode}>
-            {t('room.join.room.code', { roomCode: session?.room?.id })}
-          </Text>
-          <ShareRoomLink />
-          <StartPartyButton />
-        </View>
-        <View style={styles.infos}>
-          {/*  <RoomMissions />
+        <Image
+          source={require('../../assets/images/island.png')}
+          style={styles.image}
+        />
+        <Text style={styles.title}>{t('room.welcome.title')}</Text>
+        <Text style={styles.joinRoomCode}>
+          {t('room.join.room.code', { roomCode })}
+        </Text>
+        <ShareRoomLink />
+        <StartPartyButton />
+        <RoomMissions roomCode={roomCode} />
+        {/*  
           <CanStartParty /> */}
-        </View>
       </View>
       <View style={styles.features}>
         {/* <PlayerMissions />
