f7c903828f12ffe490bb3bf5fd4a0ae38bf3a40b
Moussa Iskounene <moussa.iskounene@gmail.com>
2023-07-15 17:47:13 +0200

    Use context for useRoom() API to correctly expose the global provider

--- packages/webservices/source/services/room/queries.ts
+++ packages/webservices/source/services/room/queries.ts
@@ -1,10 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
 
+import { context } from '../../provider';
+
 import { getRoomRequest } from './requests';
 import { type RoomQuery } from './types';
 
 export function useRoom(roomCode: string): RoomQuery {
   const { data: room, refetch: refetchRoom } = useQuery({
+    context,
     queryKey: ['room', roomCode],
     queryFn: () => getRoomRequest(roomCode),
     /* enabled: Boolean(localStorage.getItem('token')), */
