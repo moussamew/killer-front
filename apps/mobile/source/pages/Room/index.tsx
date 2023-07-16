b05b57f8a263dd5ebba6c86a071194aeb4c24722
Moussa Iskounene <moussa.iskounene@gmail.com>
2023-07-16 13:33:20 +0200

    React to events send by BE with SSE

--- apps/mobile/source/pages/Room/index.tsx
+++ apps/mobile/source/pages/Room/index.tsx
@@ -1,6 +1,14 @@
 import { useTranslation } from '@killerparty/intl';
+import {
+  ROOM_TOPIC,
+  type Room,
+  useRoom,
+  useSession,
+} from '@killerparty/webservices';
 import { type NativeStackScreenProps } from '@react-navigation/native-stack';
+import { useEffect } from 'react';
 import { Text, View, Image, ScrollView } from 'react-native';
+import EventSource from 'react-native-sse';
 
 import { type RootStackParamList } from '../../app/routes';
 
@@ -17,8 +25,34 @@ type Props = NativeStackScreenProps<RootStackParamList, 'Room'>;
 
 export function PendingRoomPage({ route }: Props): JSX.Element | null {
   const { t } = useTranslation();
-
   const { roomCode } = route.params;
+  const { session, refetchSession } = useSession();
+  const { refetchRoom } = useRoom(roomCode);
+
+  /**
+   * Listen to SSE events emits in the Room page.
+   */
+  useEffect(() => {
+    const roomEventSource = new EventSource(`${ROOM_TOPIC}/${roomCode}`);
+
+    roomEventSource.addEventListener('message', (event) => {
+      if (event.type === 'message' && event.data) {
+        const roomInfos: Room = JSON.parse(event.data);
+
+        const isPlayerInRoom = roomInfos.players.some(
+          ({ id }) => id === session?.id,
+        );
+
+        if (isPlayerInRoom) {
+          refetchRoom().then(refetchSession);
+        } else {
+          refetchSession();
+        }
+      }
+    });
+
+    return () => roomEventSource.close();
+  }, [roomCode, session?.id, refetchSession, refetchRoom]);
 
   return (
     <ScrollView style={styles.container}>
