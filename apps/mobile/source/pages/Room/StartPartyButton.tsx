d8a44c03b5937183046ea90b09176cecc22a41c9
Moussa Iskounene <moussa.iskounene@gmail.com>
2023-07-16 14:01:29 +0200

    Implements <StartPartyButton />

--- apps/mobile/source/pages/Room/StartPartyButton.tsx
+++ apps/mobile/source/pages/Room/StartPartyButton.tsx
@@ -1,15 +1,34 @@
 import { useTranslation } from '@killerparty/intl';
+import { useRoom, useStartParty } from '@killerparty/webservices';
 
 import { Button } from '../../components/Button';
 
-export function StartPartyButton(): JSX.Element {
+interface Props {
+  roomCode: string;
+}
+
+export function StartPartyButton({ roomCode }: Props): JSX.Element {
   const { t } = useTranslation();
+  const { startParty } = useStartParty();
+  const { room } = useRoom(roomCode);
+
+  const handleStartParty = (): void => {
+    startParty.mutateAsync(roomCode!);
+  };
+
+  const allPlayersHasProposedMission = room?.players.every(
+    ({ hasAtLeastOneMission }) => hasAtLeastOneMission,
+  );
 
   return (
     <Button
+      onPress={handleStartParty}
       color="primary"
-      disabled
-      onPress={() => {}}
+      disabled={
+        !room?.hasEnoughMissions ||
+        !room?.hasEnoughPlayers ||
+        !allPlayersHasProposedMission
+      }
       text={t('room.start.party.button')}
     />
   );
