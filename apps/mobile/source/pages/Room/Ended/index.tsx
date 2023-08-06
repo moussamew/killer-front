c48035de0ab6bb9e262892d60320ff5a8bcca9f3
Moussa Iskounene <moussa.iskounene@gmail.com>
2023-08-06 23:09:04 +0200

    Create basic ending room page

--- apps/mobile/source/pages/Room/Ended/index.tsx
+++ apps/mobile/source/pages/Room/Ended/index.tsx
@@ -1,9 +1,27 @@
-import { View, Text } from 'react-native';
+import { useTranslation } from '@killerparty/intl';
+import { useSession, useUpdatePlayer } from '@killerparty/webservices';
+import { View } from 'react-native';
+
+import { Button } from '../../../components/Button';
+
+import styles from './styles/index.module.css';
 
 export function EndedRoomPage(): JSX.Element {
+  const { updatePlayer } = useUpdatePlayer();
+  const { session } = useSession();
+  const { t } = useTranslation();
+
+  const handleLeaveRoom = (): void => {
+    updatePlayer.mutate({ id: session?.id, room: null });
+  };
+
   return (
-    <View>
-      <Text>Ended</Text>
+    <View style={styles.container}>
+      <Button
+        color="secondary"
+        onPress={handleLeaveRoom}
+        text={t('room.play.another.party.button')}
+      />
     </View>
   );
 }
