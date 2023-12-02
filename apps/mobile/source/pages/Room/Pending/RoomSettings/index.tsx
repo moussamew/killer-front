e62d2e6735df5664640aa11830121c16b938c8b8
Moussa Iskounene <moussa.iskounene@gmail.com>
2023-12-02 16:34:30 +0100

    Improve <RoomSettings />

--- apps/mobile/source/pages/Room/Pending/RoomSettings/index.tsx
+++ apps/mobile/source/pages/Room/Pending/RoomSettings/index.tsx
@@ -2,9 +2,12 @@ import { useTranslation } from '@killerparty/intl';
 import { useSession, useUpdatePlayer } from '@killerparty/webservices';
 import { type NativeStackScreenProps } from '@react-navigation/native-stack';
 import LottieView from 'lottie-react-native';
-import { View } from 'react-native';
+import { createRef, useState } from 'react';
+import { type TextInput, View } from 'react-native';
 
 import { Button } from '../../../../components/Button';
+import { Input } from '../../../../components/Input';
+import { Player } from '../../../../components/Player';
 import { RoomGuard } from '../../../../components/RoomGuard';
 import { type RootStackParamList } from '../../../../types/navigation';
 
@@ -13,6 +16,8 @@ import styles from './styles/index.module.css';
 type Props = NativeStackScreenProps<RootStackParamList, 'RoomSettings'>;
 
 export function RoomSettings({ route }: Props): JSX.Element {
+  const [pseudo, setPseudo] = useState('');
+  const inputRef = createRef<TextInput>();
   const { updatePlayer } = useUpdatePlayer();
   const { session } = useSession();
   const { t } = useTranslation();
@@ -25,6 +30,14 @@ export function RoomSettings({ route }: Props): JSX.Element {
     updatePlayer.mutate({ id: session?.id, room: null });
   };
 
+  const handleUpdatePseudo = async (): Promise<void> => {
+    await updatePlayer.mutateAsync({
+      id: session?.id,
+
+      name: pseudo,
+    });
+  };
+
   return (
     <RoomGuard roomCode={roomCode} currentRouteName={routeName}>
       <View style={styles.content}>
@@ -33,6 +46,31 @@ export function RoomSettings({ route }: Props): JSX.Element {
           autoPlay
           style={styles.lottie}
           loop
+        />
+        {session && (
+          <Player
+            player={{
+              id: session.id,
+              name: session.name,
+              avatar: session.avatar,
+              roomCode,
+              hasAtLeastOneMission: Boolean(session.authoredMissions.length),
+            }}
+          />
+        )}
+
+        <Input
+          innerRef={inputRef}
+          label="Modifier le pseudo de votre joueur"
+          value={pseudo}
+          setValue={setPseudo}
+        />
+        <Button
+          disabled={!pseudo}
+          color="secondary"
+          onPress={handleUpdatePseudo}
+          text="Mettre à jour mon pseudo"
+          isAsyncAction
         />
         <Button
           color="primary"
