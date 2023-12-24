877b4c3b74463aeb1ba4f1f3b4d042e177d45729
Moussa Iskounene <moussa.iskounene@gmail.com>
2023-12-24 01:58:41 +0100

    Add killing player feature for game master

--- apps/mobile/source/pages/Room/Pending/RoomPlayers/PlayerModal.tsx
+++ apps/mobile/source/pages/Room/Pending/RoomPlayers/PlayerModal.tsx
@@ -1,4 +1,4 @@
-import { useUpdatePlayer } from '@killerparty/webservices';
+import { useSession, useUpdatePlayer } from '@killerparty/webservices';
 import { useNavigation } from '@react-navigation/native';
 import { type NativeStackScreenProps } from '@react-navigation/native-stack';
 import LottieView from 'lottie-react-native';
@@ -12,14 +12,21 @@ import {
   type RootStackParamList,
 } from '../../../../types/navigation';
 
-import styles from './styles/PlayerList.module.css';
+import styles from './styles/PlayerModal.module.css';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'PlayerModal'>;
 
 export function PlayerModal({ route }: Props): JSX.Element {
   const { updatePlayer } = useUpdatePlayer();
   const { popToTop } = useNavigation<StackNavigation>();
+  const { session } = useSession();
   const { player } = route.params;
+
+  const handleKillPlayer = async (playerId: number): Promise<void> => {
+    await updatePlayer.mutateAsync({ id: playerId, status: 'KILLED' });
+
+    popToTop();
+  };
 
   const handleKickPlayer = async (playerId: number): Promise<void> => {
     await updatePlayer.mutateAsync({ id: playerId, room: null });
@@ -30,20 +37,32 @@ export function PlayerModal({ route }: Props): JSX.Element {
   return (
     <>
       <Header shouldHandlePreviousPage={false} title="Informations du joueur" />
-      <View style={{ margin: 20 }}>
+      <View style={styles.content}>
         <LottieView
-          source={require('../../../../assets/lotties/player-infos.json')}
+          source={require('../../../../assets/lotties/free-for-all.json')}
           autoPlay
           style={styles.lottie}
           loop
         />
-        <Player player={player} />
-        <Button
-          color="primary"
-          text="Expulser ce joueur de la partie"
-          onPress={() => handleKickPlayer(player.id)}
-          isAsyncAction
-        />
+        <View style={styles.playerInfos}>
+          <Player player={player} />
+          <View>
+            {session?.status === 'SPECTATING' && player.status === 'ALIVE' && (
+              <Button
+                color="secondary"
+                text="Provoquer la mort de ce joueur"
+                onPress={() => handleKillPlayer(player.id)}
+                isAsyncAction
+              />
+            )}
+            <Button
+              color="primary"
+              text="Expulser ce joueur de la partie"
+              onPress={() => handleKickPlayer(player.id)}
+              isAsyncAction
+            />
+          </View>
+        </View>
       </View>
     </>
   );
