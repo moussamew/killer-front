e13753be4478c0e9b1348d7df7b1c96f5712545c
Moussa Iskounene <m1skou@pm.me>
2024-05-02 01:51:44 +0200

    Add SSL

--- apps/web/config/plugins.ts
+++ apps/web/config/plugins.ts
@@ -1,8 +1,10 @@
 import react from '@vitejs/plugin-react';
+import mkcert from 'vite-plugin-mkcert';
 import svgr from 'vite-plugin-svgr';
 
 export const Plugins = [
   react(),
+  mkcert({ hosts: ['localhost', 'dev.killerparty.app'] }),
   svgr({
     exportAsDefault: true,
     svgrOptions: { titleProp: true },
