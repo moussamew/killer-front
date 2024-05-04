b941d797cf8fd7d510fe77ac8f8d8aa2a66ca5b2
Moussa Iskounene <m1skou@pm.me>
2024-05-04 16:10:21 +0200

    Refactoring create room

--- apps/web/source/pages/Home/CreateRoomV2.tsx
+++ apps/web/source/pages/Home/CreateRoomV2.tsx
@@ -1,23 +1,13 @@
 import { useTranslation } from '@killerparty/intl';
-import { LoaderCircle, PlusCircle, SquarePen } from 'lucide-react';
-import { useEffect, useMemo, useRef, useState } from 'react';
-import { toast } from 'sonner';
+import { PlusCircle } from 'lucide-react';
+import { useState } from 'react';
 
-import { avatarList, Gallery } from '@/components/Gallery';
 import { Button } from '@/components/ui/Button';
-import { type CarouselApi } from '@/components/ui/Carousel';
 import { Drawer, DrawerContent, DrawerTrigger } from '@/components/ui/Drawer';
-import { Input } from '@/components/ui/Input';
-import { Typography } from '@/components/ui/Typography';
-import { cn } from '@/lib/utils';
-import { useCreatePlayer, useUpdatePlayer } from '@/services/player/mutations';
-import { useSession } from '@/services/player/queries';
-import { useCreateRoom } from '@/services/room/mutations';
 
 import { ChooseGameMode } from './ChooseGameMode';
 import { type Mode, modes } from './constants';
-import { GameCarousel } from './GameCarousel';
-import styles from './styles/CreateRoomV2.module.css';
+import { CreatePlayer } from './CreatePlayer';
 
 interface Props {
   defaultAvatar: string;
@@ -26,66 +16,12 @@ interface Props {
 
 export function CreateRoomV2({ defaultAvatar, setDefaultAvatar }: Props) {
   const { t } = useTranslation();
-  const { session } = useSession();
-  const inputRef = useRef<HTMLInputElement>(null);
-  const [pseudo, setPseudo] = useState<string | null>(session?.name ?? null);
-  const [isModeOpen, setIsModeOpen] = useState(false);
+
   const [mode, setMode] = useState<Mode>(modes[0]);
-  const [carouselApi, setCarouselApi] = useState<CarouselApi>();
-  const [open, setOpen] = useState(false);
-  const { createPlayer } = useCreatePlayer();
-  const { updatePlayer } = useUpdatePlayer();
-  const { createRoom } = useCreateRoom();
-
-  const currentAvatar = useMemo(
-    () => session?.avatar ?? defaultAvatar,
-    [session, defaultAvatar],
-  );
-
-  useEffect(() => {
-    if (carouselApi) {
-      carouselApi.scrollTo(mode.id);
-
-      carouselApi.on('select', ({ selectedScrollSnap }) => {
-        const selectedScroll = selectedScrollSnap();
-
-        if (mode.id !== selectedScroll) {
-          setMode(modes[selectedScroll]);
-        }
-      });
-    }
-  }, [carouselApi, mode]);
-
-  const handleCreateRoom = async (): Promise<void> => {
-    if (!pseudo) return;
-
-    if (session) {
-      await updatePlayer.mutateAsync({
-        id: session.id,
-        name: pseudo,
-        avatar: currentAvatar,
-      });
-    } else {
-      await createPlayer.mutateAsync({
-        name: pseudo,
-        avatar: currentAvatar,
-      });
-    }
-
-    await createRoom.mutateAsync({
-      isGameMastered: Boolean(mode.value === 'game master'),
-    });
-
-    toast.success('Nouvelle partie créée avec succès');
-
-    setOpen(false);
-  };
-
-  const isCreationRoomPending =
-    createPlayer.isPending || updatePlayer.isPending || createRoom.isPending;
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
   return (
-    <Drawer open={open} onOpenChange={setOpen}>
+    <Drawer open={drawerOpen} onOpenChange={setDrawerOpen}>
       <DrawerTrigger asChild>
         <Button size="lg">
           {t('home.create.room.button')}
@@ -93,83 +29,14 @@ export function CreateRoomV2({ defaultAvatar, setDefaultAvatar }: Props) {
         </Button>
       </DrawerTrigger>
       <DrawerContent title="Création de partie">
-        <div className="lg:grid mx-8 m-auto lg:grid-cols-2 gap-8">
-          <div className="flex items-start justify-center shadow-md rounded-lg p-8 bg-brand">
-            <div className="mx-auto grid gap-6">
-              <div className="grid gap-2 text-center" />
-              <div className={styles.avatar}>{avatarList[currentAvatar]}</div>
-              <Typography.H3
-                className={cn(
-                  'text-center text-slate-500 cursor-pointer hover:text-slate-800 transition duration-500 hover:scale-105 flex items-center justify-center',
-                  { 'text-slate-800': pseudo },
-                )}
-                onClick={() => inputRef.current?.focus()}
-              >
-                <>
-                  {pseudo || '[Choisir un nom..]'}
-                  {pseudo && <SquarePen className="ml-2" />}
-                </>
-              </Typography.H3>
-              <Gallery
-                currentAvatar={currentAvatar}
-                setCurrentAvatar={setDefaultAvatar}
-              />
-              <div className="flex flex-col">
-                <div className="flex flex-row gap-4 mb-4">
-                  <Input
-                    ref={inputRef}
-                    id="pseudo"
-                    type="text"
-                    placeholder="Bruce Wayne"
-                    required
-                    className="w-1/2"
-                    value={pseudo ?? ''}
-                    onChange={(e) => setPseudo(e.target.value)}
-                  />
-                  <ChooseGameMode
-                    isModeOpen={isModeOpen}
-                    mode={mode}
-                    setIsModeOpen={setIsModeOpen}
-                    setMode={setMode}
-                  />
-                </div>
-                <Button
-                  disabled={!pseudo || isCreationRoomPending}
-                  onClick={handleCreateRoom}
-                  size="lg"
-                  className="transition-opacity ease-in duration-500"
-                >
-                  {isCreationRoomPending ? (
-                    <>
-                      <LoaderCircle className="mr-2 h-4 w-4 animate-spin" />
-                      Création en cours..
-                    </>
-                  ) : (
-                    t('home.create.room.confirm.button')
-                  )}
-                </Button>
-              </div>
-            </div>
-          </div>
-          <div className="shadow-md rounded-lg p-8 bg-brand">
-            {mode.value === 'game master' && (
-              <div className="text-center">
-                <Typography.H3 className="my-4">
-                  {t('create.room.game.master.mode.title')}
-                </Typography.H3>
-                <p>{t('create.room.game.master.mode.description')}</p>
-              </div>
-            )}
-            {mode.value === 'player' && (
-              <div className=" text-center">
-                <Typography.H3 className="my-4">
-                  {t('create.room.free.for.all.mode.title')}
-                </Typography.H3>
-                <p>{t('create.room.free.for.all.mode.description')}</p>
-              </div>
-            )}
-            <GameCarousel setApi={setCarouselApi} />
-          </div>
+        <div className="flex mx-8 m-auto gap-8">
+          <CreatePlayer
+            defaultAvatar={defaultAvatar}
+            setDefaultAvatar={setDefaultAvatar}
+            mode={mode}
+            setDrawerOpen={setDrawerOpen}
+          />
+          <ChooseGameMode mode={mode} setMode={setMode} />
         </div>
       </DrawerContent>
     </Drawer>
