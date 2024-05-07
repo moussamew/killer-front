b927d706d1d754d4c4ad5aa37325835b8c8abc6c
Moussa Iskounene <m1skou@pm.me>
2024-05-07 17:30:02 +0200

    Add and improve components from shadcn

--- apps/web/source/components/ui/Button.tsx
+++ apps/web/source/components/ui/Button.tsx
@@ -5,7 +5,7 @@ import { type ButtonHTMLAttributes, forwardRef } from 'react';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-m font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 tracking-tight',
+  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-m font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 tracking-tight transition-opacity ease-in duration-500',
   {
     variants: {
       variant: {
@@ -16,7 +16,7 @@ const buttonVariants = cva(
           'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
         secondary:
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
+        ghost: 'hover:bg-brand hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
--- apps/web/source/components/ui/Progress.tsx
+++ apps/web/source/components/ui/Progress.tsx
@@ -10,13 +10,17 @@ const Progress = React.forwardRef<
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
-      'relative h-4 w-full overflow-hidden cursor-pointer rounded-full bg-white border-brand border-gray-200 border-[1px] hover:scale-105 transition duration-500',
+      'relative h-4 w-full overflow-hidden cursor-pointer rounded-full bg-white border-brand border-gray-200 border-[1px]',
       className,
     )}
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-red-500 transition-all"
+      className={cn('h-full w-full flex transition-all', {
+        'bg-green-500/50': value && value >= 100,
+        'bg-orange-500/50': value && value < 70,
+        'bg-red-500/50': value && value < 30,
+      })}
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
