6a6dc229848dc320a0b504913fcc647dcea6e81f
Moussa Iskounene <m1skou@pm.me>
2024-05-07 17:30:02 +0200

    Add and improve components from shadcn

--- apps/web/source/components/ui/Input.tsx
+++ apps/web/source/components/ui/Input.tsx
@@ -13,8 +13,8 @@ const Input = forwardRef<
         `flex h-10 w-full rounded-md border border-input 
           bg-background px-3 py-2 text-m ring-offset-background 
           file:border-0 file:bg-transparent file:text-sm file:font-medium 
-          placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 
-          focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed 
+          placeholder:text-muted-foreground/50 focus-visible:outline-none focus-visible:ring-1
+          disabled:cursor-not-allowed 
           disabled:opacity-50`,
         className,
       )}
