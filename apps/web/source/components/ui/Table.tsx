2d31966679a122c52137cee44b48fbf7aa477ed0
Moussa Iskounene <m1skou@pm.me>
2024-05-09 14:44:29 +0200

    Improve some ui components

--- apps/web/source/components/ui/Table.tsx
+++ apps/web/source/components/ui/Table.tsx
@@ -62,7 +62,7 @@ const TableRow = React.forwardRef<
   <tr
     ref={ref}
     className={cn(
-      'border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted',
+      'border-b transition-colors hover:bg-brand/80 data-[state=selected]:bg-muted',
       className,
     )}
     {...props}
